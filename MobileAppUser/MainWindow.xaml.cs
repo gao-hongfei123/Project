340858aa4d67b2d030d093b558d310fff7927b24
gao-hongfei123 <303515096@qq.com>
2023-04-08 14:03:57 +0800

    MaterialDesignWpfTest1

---
+++ MobileAppUser/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MobileAppUser
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dragMe(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void todayBtnClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void weekBtnClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void monthBtnClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
